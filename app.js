442d8271682068fdc0bb850e8ec64effb5a879ea
Debbie <debbiecamera0704@gmail.com>
2021-05-08 10:25:04 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -1,26 +1,12 @@
 const express = require('express')
-const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
+require('./config/mongoose')
 
-const Restaurant = require('./models/restaurant')
 const routes = require('./routes')
 
 const app = express()
-
-
-mongoose.connect('mongodb://localhost/restaurant_list', { useNewUrlParser: true, useUnifiedTopology: true })
-
-const db = mongoose.connection
-
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-
-db.once('open', () => {
-  console.log('mongodb ready!')
-})
 
 app.use(bodyParser.urlencoded({ extended: true }))
 
--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -1,17 +1,8 @@
-const mongoose = require('mongoose')
 const Restaurant = require('../restaurant')
 const restaurantList = require('./restaurant.json')
-
-mongoose.connect('mongodb://localhost/restaurant_list', { useNewUrlParser: true })
-const db = mongoose.connection
-
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-
+const db = require('../../config/mongoose')
 
 db.once('open', () => {
-  console.log('mongodb ready!')
   restaurantList.results.forEach((item) => {
     Restaurant.create(item)
   })
