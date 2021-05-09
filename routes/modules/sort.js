736e0af907191bd12187513e7438228f6db85e27
Debbie <debbiecamera0704@gmail.com>
2021-05-10 01:18:13 +0800

    fix bugs in sort.js

--- routes/modules/sort.js
+++ routes/modules/sort.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const Restaurant = require('../../models/restaurant')
 
 const sortValues = ['A-Z', 'Z-A', '食物類別', '上傳時間', '評價高低']
-const sortTypes = [{ name: 'asc' }, { name: 'desc' }, { category: 'asc' }, { _id: 'asc' }, { rating: 'desc' }]
+const sortTypes = [{ name: 'asc' }, { name: 'desc' }, { category: 'asc' }, { _id: 'desc' }, { rating: 'desc' }]
 const sortObj = {}
 
 for (i = 0; i < 5; i++) {
