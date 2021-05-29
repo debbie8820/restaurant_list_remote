31dec8d1280f2aae32e73fef6e7613a1ecb3a4d9
Debbie <debbiecamera0704@gmail.com>
2021-05-29 14:27:27 +0800

    feat: add user registration feature

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
+
 require('./config/mongoose')
 
 const routes = require('./routes')
 
 const app = express()
 app.use(bodyParser.urlencoded({ extended: true }))
+
 
 app.engine('handlebars', exphbs({
   defaultLayout: "main",
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const router = express.Router()
+const bcrypt = require('bcryptjs')
+const User = require('../../models/user')
 
 router.get('/login', (req, res) => {
   res.render('login')
@@ -14,7 +16,32 @@ router.get('/register', (req, res) => {
 })
 
 router.post('/register', (req, res) => {
-
+  const { name, email, password, confirmPassword } = req.body
+  if (!email || !password || !confirmPassword) {
+    console.log('有欄位沒填寫')
+    return res.render('register', { name, email, password, confirmPassword })
+  }
+  if (password !== confirmPassword) {
+    console.log('密碼和確認密碼不符合')
+    return res.render('register', { name, email, password, confirmPassword })
+  }
+  return User.findOne({ email })
+    .then(user => {
+      if (user) {
+        return console.log('此 Email 已被註冊')
+      }
+      return bcrypt.genSalt(10)
+        .then(salt => bcrypt.hash(password, salt))
+        .then(hash => {
+          User.create({
+            name,
+            email,
+            password: hash
+          })
+        })
+        .then(() => res.redirect('/users/login'))
+        .catch(err => console.log(err))
+    })
 })
 
 module.exports = router
