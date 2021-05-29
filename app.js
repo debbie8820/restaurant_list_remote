216196940c0be0d4d5d3275e524a2cbf411a51ae
Debbie <debbiecamera0704@gmail.com>
2021-05-29 14:51:04 +0800

    feat: add express-session

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
+const session = require('express-session')
 
 require('./config/mongoose')
 
@@ -9,6 +10,11 @@ const routes = require('./routes')
 
 const app = express()
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(session({
+  secret: 'debbieSecret', //只有伺服器知道的的密鑰
+  resave: false,
+  saveUninitialized: true
+}))
 
 
 app.engine('handlebars', exphbs({
