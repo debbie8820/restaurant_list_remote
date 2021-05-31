1a266d1688900b41bfb91578cad551adf0e4bec5
Debbie <debbiecamera0704@gmail.com>
2021-05-31 11:42:51 +0800

    modify Seeders

--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -1,13 +1,23 @@
 const Restaurant = require('../restaurant')
 const restaurantList = require('./restaurant.json')
 const db = require('../../config/mongoose')
+const User = require('../../models/user')
+const userList = require('./users.json')
 
-db.once('open', () => {
-  restaurantList.results.forEach((item) => {
-    Restaurant.create(item)
-  })
-  console.log('Seeds created')
+db.once('open', async () => {
+  try {
+    for (let i = 0; i < 2; i++) {
+      await User.findOne({ email: userList[i].email })
+        .then((user) => {
+          return Promise.all(restaurantList.results.slice(3 * i, 3 * (i + 1)).map(async (items) => {
+            items.userId = user._id
+            return await Restaurant.create(items)
+          }))
+        })
+    }
+  } catch (err) {
+    console.log(err)
+  }
+  console.log('Seeds are created')
+  process.exit()
 })
-
-
-
