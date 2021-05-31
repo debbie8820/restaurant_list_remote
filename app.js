48d77f49196166f231f1d8a38629cdfcb2dca34c
Debbie <debbiecamera0704@gmail.com>
2021-05-31 12:09:34 +0800

    feat: add connect flash to login page

--- app.js
+++ app.js
@@ -43,6 +43,7 @@ app.use((req, res, next) => {
   res.locals.user = req.user
   res.locals.warning_msg = req.flash('warning_msg')
   res.locals.success_msg = req.flash('success_msg')
+  res.locals.danger_msg = req.flash('danger_msg')
   next()
 })
 
