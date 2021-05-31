2ca826223e17e8eab3ee37ea7526e8c17b5d3168
Debbie <debbiecamera0704@gmail.com>
2021-05-31 15:15:53 +0800

    Revert "feat: add facebook login"
    This reverts commit d87decddae2298e2b9dd1b0a6666364234bdc0fa.
    
    # Conflicts:
    #	config/passport.js

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,14 @@
 const express = require('express')
 const router = express.Router()
-
 const home = require('./modules/home')
 const restaurant = require('./modules/restaurant')
 const users = require('./modules/users')
 const sort = require('./modules/sort')
-const auth = require('./modules/auth')
-
 const { authenticator } = require('../middleware/auth')
 
 router.use('/restaurants', authenticator, restaurant)
 router.use('/sort', authenticator, sort)
 router.use('/users', users)
-router.use('/auth', auth)
 router.use('/', authenticator, home)
 
 module.exports = router
