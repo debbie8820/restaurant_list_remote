94227e9eb634838cea9bf0ecb261c96fda3c731b
Debbie <debbiecamera0704@gmail.com>
2021-06-20 19:35:02 +0800

    rewrite codes into async/await style

--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -7,17 +7,15 @@ const userList = require('./users.json')
 db.once('open', async () => {
   try {
     for (let i = 0; i < 2; i++) {
-      await User.findOne({ email: userList[i].email })
-        .then((user) => {
-          return Promise.all(restaurantList.results.slice(3 * i, 3 * (i + 1)).map(async (items) => {
-            items.userId = user._id
-            return await Restaurant.create(items)
-          }))
-        })
+      const user = await User.findOne({ email: userList[i].email })
+      return Promise.all(restaurantList.results.slice(3 * i, 3 * (i + 1)).map(async (items) => {
+        items.userId = user._id
+        return await Restaurant.create(items)
+      }))
     }
   } catch (err) {
-    console.log(err)
+    return console.log(err)
   }
   console.log('Seeds are created')
-  process.exit()
+  return db.close()
 })
